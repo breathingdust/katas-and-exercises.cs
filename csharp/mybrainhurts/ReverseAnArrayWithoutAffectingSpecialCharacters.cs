ef04ce71901d0608cd67f459756f65637102a0f3
Simon Davis <simon@breathingdust.com>
2016-08-26 08:23:39 -0700

    next problem finished

--- csharp/mybrainhurts/ReverseAnArrayWithoutAffectingSpecialCharacters.cs
+++ csharp/mybrainhurts/ReverseAnArrayWithoutAffectingSpecialCharacters.cs
@@ -18,32 +18,75 @@ namespace mybrainhurts
     [TestFixture]
     public class ReverseAnArrayWithoutAffectingSpecialCharacters
     {
-        [Test]
-        public void ReverseWithoutAffectingSpecialCharacters()
+        /// <summary>
+        /// If both positions are non special then swap
+        /// if the first position is special and the last is not increment first position
+        /// if the first position is non-special and the last is special then increment last position
+        /// if both are special then increment both
+        /// 
+        /// Time: O(n) Space: O(1)
+        /// </summary>
+
+        [TestCase("a,b$c", "c,b$a")]
+        [TestCase("ab,c$d", "dc,b$a")]
+        [TestCase("#$%^", "#$%^")]
+        [TestCase("abcd", "dcba")]
+        public void ReverseWithoutAffectingSpecialCharacters(string input, string expected)
         {
-            var input = "a,b$c".ToCharArray();
-            var expected = "c,b$a";
+            var inputArray = input.ToCharArray();
 
             var forwardIndex = 0;
-            var reverseIndex = input.Length - 1;
+            var reverseIndex = inputArray.Length - 1;
 
-            for (var i = 0; i < input.Length; i++){
-                var current = input[i];
-                if (!IsASpecialCharacter(input[forwardIndex]) && !IsASpecialCharacter(input[reverseIndex]))
+            while (forwardIndex < reverseIndex)
+            {
+                if (!IsASpecialCharacter(inputArray[forwardIndex]) && !IsASpecialCharacter(inputArray[reverseIndex]))
                 {
+                    var first = inputArray[forwardIndex];
+                    inputArray[forwardIndex] = inputArray[reverseIndex];
+                    inputArray[reverseIndex] = first;
+                    forwardIndex++;
+                    reverseIndex--;
+                }
+                else
+                {
+                    if (IsASpecialCharacter(inputArray[forwardIndex]))
+                    {
+                        forwardIndex++;
+                    }
 
+                    if (IsASpecialCharacter(inputArray[reverseIndex]))
+                    {
+                        reverseIndex--;
+                    }
                 }
             }
 
-            var actual = new string(input);
+            var actual = new string(inputArray);
 
             Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void matches_a_special_character()
+        {
+            char input = '%';
+
+            Assert.That(IsASpecialCharacter(input), Is.True);
+        }
+
+        [Test]
+        public void does_not_match_a_special_character()
+        {
+            char input = 'a';
+
+            Assert.That(IsASpecialCharacter(input), Is.False);
         }
 
         public bool IsASpecialCharacter(char character)
         {
             var regex = new Regex(@"^\w+$");
-            return regex.IsMatch(character.ToString());
+            return !regex.IsMatch(character.ToString());
         }
     }
 
