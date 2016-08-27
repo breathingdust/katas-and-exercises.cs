a9046dfba745add7fdedacd44b9e2987a816cfd9
Simon Davis <simon@breathingdust.com>
2016-08-26 22:19:48 -0700

    an n^2 solution

--- csharp/mybrainhurts/CountTripletsWithSumSmallerThanAGivenValue.cs
+++ csharp/mybrainhurts/CountTripletsWithSumSmallerThanAGivenValue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
@@ -13,6 +14,15 @@ namespace mybrainhurts
         public void CountTriplets(int[] input, int threshold, int expected)
         {
             var actual = DoTheThing(input, threshold);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(new[] { -2, 0, 1, 3 }, 2, 2)]
+        [TestCase(new[] { 5, 1, 3, 4, 7 }, 12, 4)]
+        public void CountTripletsBetter(int[] input, int threshold, int expected)
+        {
+            var actual = DoTheThingBetter(input, threshold);
 
             Assert.That(actual, Is.EqualTo(expected));
         }
@@ -38,6 +48,34 @@ namespace mybrainhurts
                         }
                     }
                 }
+            }
+            return result;
+        }
+
+        private static int DoTheThingBetter(int[] input, int threshold)
+        {
+            var result = 0;
+            input = input.OrderBy(o => o).ToArray();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                int j = i + 1;
+                int k = input.Length - 1;
+
+                while (j < k)
+                {
+                    if (input[i] + input[j] + input[k] >= threshold)
+                    {
+                        k--;
+                    }
+                    else
+                    {
+                        result += (k - j);
+                        j++;
+                    }
+                }
+
+
             }
             return result;
         }
